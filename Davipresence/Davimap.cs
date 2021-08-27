de6e8a44c04039d506c16359fbb041744adfceb2
SquirrelKiev <laidbackenon@outlook.com>
2021-08-27 20:48:49 +0100

    Davimap definition

--- Davipresence/Davimap.cs
+++ Davipresence/Davimap.cs
@@ -4,5 +4,13 @@ namespace Davipresence
 {
     class Davimap
     {
+        public string mapID, assetKey, displayName;
+
+        public Davimap(string mapID, string assetKey, string displayName)
+        {
+            this.mapID = mapID;
+            this.assetKey = assetKey;
+            this.displayName = displayName;
+        }
     }
 }
