6ff2f472be3b8e31a4fc6b739d9ea9190fbff0a0
SquirrelKiev <laidbackenon@outlook.com>
2021-08-27 15:44:08 +0100

    More MelonLoader

--- Davipresence/DP.cs
+++ Davipresence/DP.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MelonLoader
 
 namespace Davipresence
 {
-    public class DP
+    public class DP : MelonMod
     {
     }
 }
